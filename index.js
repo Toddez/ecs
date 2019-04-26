10d610b8d09e8ac2a2e3b78eb9f23158e6fe83ff
Todde <todde.apache@gmail.com>
2019-04-26 13:31:36 +0200

    More

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 global.rootDir = __dirname;
 
-const fs = require('fs');
 const express = require('express');
 
-var app = express();
-app.use(express.static(global.rootDir + '/public'));
+const app = express();
+app.use(express.static(`${global.rootDir}/public`));
 app.set('view engine', 'ejs');
-app.set('views', global.rootDir + '/views');
+app.set('views', `${global.rootDir}/views`);
 
 app.get('/*', function(req, res, next) {
-    res.render('index.ejs', {}, function(err, html) {
-        res.send(html);
-    });
+  res.render('index.ejs', {}, function(err, html) {
+    res.send(html);
+  });
 });
 
 app.listen(80, function() {
-    console.log('Server started');
+  console.log('Server started');
 });
